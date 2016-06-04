9eaf718619c4d8bb5463aef7bd20497a5db9d420
ComBoro <Kalin.p.Krustev@gmail.com>
2016-06-04 10:48:05 +0300

    Hello World

---
+++ HelloW.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class HelloW {
+	public static void main(String[] args) {
+		JFrame fr = new JFrame("Working");
+		fr.setSize(800,600);
+		fr.setDefaultCloseOperation(3);
+		fr.setTitle("Aj mi kura georgi");
+		fr.setVisible(true);
+	}
+}
